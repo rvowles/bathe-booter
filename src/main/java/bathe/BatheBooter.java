6af2222044ca6718837ac27273d1dc8eae8302e7
pcummuskey <pcummuskey@bravurasolutions.com>
2019-02-15 14:49:40 +1300

    #4: Normalising whitespace.

--- src/main/java/bathe/BatheBooter.java
+++ src/main/java/bathe/BatheBooter.java
@@ -25,99 +25,99 @@ import java.util.jar.Manifest;
  * author: Richard Vowles - http://gplus.to/RichardVowles
  */
 public class BatheBooter {
-  private static final String WEB_JAR_PREFIX = "WEB-INF/jars/";
-  private static final String WEB_CLASSES_PREFIX = "WEB-INF/classes/";
+	private static final String WEB_JAR_PREFIX = "WEB-INF/jars/";
+	private static final String WEB_CLASSES_PREFIX = "WEB-INF/classes/";
 
-  private static final String MAIN_OVERRIDE = "-R";
-  private static final String JUMP_CLASS = "Jump-Class";
-  protected static final String BATHE_EXTERNAL_CLASSPATH = "bathe.externalClassPath";
+	private static final String MAIN_OVERRIDE = "-R";
+	private static final String JUMP_CLASS = "Jump-Class";
+	protected static final String BATHE_EXTERNAL_CLASSPATH = "bathe.externalClassPath";
 	private static final String BATHE_IMPLEMENTATION_VERSION = "Bathe-Implementation-Version";
 
 	protected String runnerClass;
-  protected File jar;
-  protected String[] passingArgs;
+	protected File jar;
+	protected String[] passingArgs;
 
-  protected void parseCommandLine(String[] args) {
-    List<String> appArguments = new ArrayList<String>();
+	protected void parseCommandLine(String[] args) {
+		List<String> appArguments = new ArrayList<String>();
 
-    // Process command line arguments
-    for (String arg : args) {
-      if (arg.startsWith(MAIN_OVERRIDE)) {
-        runnerClass = arg.substring(MAIN_OVERRIDE.length());
-      } else
-        appArguments.add(arg);
-    }
+		// Process command line arguments
+		for (String arg : args) {
+			if (arg.startsWith(MAIN_OVERRIDE)) {
+				runnerClass = arg.substring(MAIN_OVERRIDE.length());
+			} else
+				appArguments.add(arg);
+		}
 
-    if (runnerClass == null) {
-      attemptToGetJumpClassFromManifest();
-    }
+		if (runnerClass == null) {
+			attemptToGetJumpClassFromManifest();
+		}
 
-    if (appArguments.size() > 0 && runnerClass == null) {
-      System.err.println(
-        "Usage: java -jar " + jar.getName() + " [options]\n" +
-          "\n" +
-          "Arguments:\n" +
-          "  " + MAIN_OVERRIDE + "<class-name>      specify the main class.\n" +
-          "  \n" +
-          "Specify Jump-Class: in your /META-INF/MANIFEST.MF to automatically define a class to run");
+		if (appArguments.size() > 0 && runnerClass == null) {
+			System.err.println(
+					"Usage: java -jar " + jar.getName() + " [options]\n" +
+					"\n" +
+					"Arguments:\n" +
+					"  " + MAIN_OVERRIDE + "<class-name>      specify the main class.\n" +
+					"  \n" +
+					"Specify Jump-Class: in your /META-INF/MANIFEST.MF to automatically define a class to run");
 
-      System.exit(-2);
-    }
+			System.exit(-2);
+		}
 
 
-    passingArgs = new String[appArguments.size()];
+		passingArgs = new String[appArguments.size()];
 
-    appArguments.toArray(passingArgs);
-  }
+		appArguments.toArray(passingArgs);
+	}
 
-  protected void attemptToGetJumpClassFromManifest() {
-    try {
-      Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
-        .getResources("META-INF/MANIFEST.MF");
+	protected void attemptToGetJumpClassFromManifest() {
+		try {
+			Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
+					.getResources("META-INF/MANIFEST.MF");
 
-      while (resources.hasMoreElements()) {
-        Manifest manifest = new Manifest(resources.nextElement().openStream());
+			while (resources.hasMoreElements()) {
+				Manifest manifest = new Manifest(resources.nextElement().openStream());
 
-        Attributes attr = manifest.getMainAttributes();
-        String jumpGate = attr.getValue(JUMP_CLASS);
+				Attributes attr = manifest.getMainAttributes();
+				String jumpGate = attr.getValue(JUMP_CLASS);
 
-        if (jumpGate != null) {
+				if (jumpGate != null) {
 
-          runnerClass = jumpGate;
+					runnerClass = jumpGate;
 
-	        System.setProperty(BATHE_IMPLEMENTATION_VERSION, attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
+					System.setProperty(BATHE_IMPLEMENTATION_VERSION, attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
 
-          break;
-        }
-      }
+					break;
+				}
+			}
 
-    } catch (IOException iex) {
+		} catch (IOException iex) {
 
-    }
+		}
 
-  }
+	}
 
-  public static void main(String []args) throws IOException {
-    new BatheBooter().run(args);
-  }
+	public static void main(String[] args) throws IOException {
+		new BatheBooter().run(args);
+	}
 
-  public void run(String []args) throws IOException {
-    // Find the WAR and load the corresponding properties
-    jar = getRunFile();
+	public void run(String[] args) throws IOException {
+		// Find the WAR and load the corresponding properties
+		jar = getRunFile();
 
-    parseCommandLine(args);
+		parseCommandLine(args);
 
-	  String externalClasspath = System.getProperty(BATHE_EXTERNAL_CLASSPATH);
+		String externalClasspath = System.getProperty(BATHE_EXTERNAL_CLASSPATH);
 
-	  // support external classpath
-	  ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		// support external classpath
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-	  if (externalClasspath != null) {
-		  classLoader = new URLClassLoader(new URL[] {new File(externalClasspath).toURI().toURL()}, classLoader);
-	  }
+		if (externalClasspath != null) {
+			classLoader = new URLClassLoader(new URL[]{new File(externalClasspath).toURI().toURL()}, classLoader);
+		}
 
-    runWithLoader(classLoader, jar, runnerClass, passingArgs);
-  }
+		runWithLoader(classLoader, jar, runnerClass, passingArgs);
+	}
 
 	public void runWithLoader(ClassLoader loader, File runnable, String runnerClass, String[] args) throws IOException {
 		ClassLoader localLoader = loader == null ? Thread.currentThread().getContextClassLoader() : loader;
@@ -150,7 +150,7 @@ public class BatheBooter {
 			Method method = runner.getMethod("main", String[].class);
 
 			// force to Object so it doesn't  try to use ...
-			method.invoke(null, (Object)args);
+			method.invoke(null, (Object) args);
 		} catch (NoSuchMethodException e) {
 			return false;
 		}
@@ -159,58 +159,58 @@ public class BatheBooter {
 	}
 
 	/*
-   * Runs the main runner class in the specified class loader, passing in
-   * the WAR being run as well as the specified command line arguments.
-   */
-  public void exec(ClassLoader loader, File runnable, String runnerClass, String[] args) {
-    try {
-      Class<?> runner = Class.forName(runnerClass, true, loader);
+	 * Runs the main runner class in the specified class loader, passing in
+	 * the WAR being run as well as the specified command line arguments.
+	 */
+	public void exec(ClassLoader loader, File runnable, String runnerClass, String[] args) {
+		try {
+			Class<?> runner = Class.forName(runnerClass, true, loader);
 
-	    if (!tryRunMethod(runner, runnable, args)) {
-		    if (!tryMainMethod(runner, args)) {
-			    throw new RuntimeException("Cannot find run or main method in " + runnerClass);
-		    }
-	    }
+			if (!tryRunMethod(runner, runnable, args)) {
+				if (!tryMainMethod(runner, args)) {
+					throw new RuntimeException("Cannot find run or main method in " + runnerClass);
+				}
+			}
 
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException("The class you are trying to run can not be found on the classpath: " + runnerClass + ", " + loader.toString(), e);
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException("Run method needs to be public and static", e);
-    } catch (InvocationTargetException e) {
-      Throwable cause = e.getCause();
-      if (cause instanceof RuntimeException) throw (RuntimeException) cause;
-      throw new RuntimeException("Unhandled exception in WebApp runner", e);
-    }
-  }
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("The class you are trying to run can not be found on the classpath: " + runnerClass + ", " + loader.toString(), e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Run method needs to be public and static", e);
+		} catch (InvocationTargetException e) {
+			Throwable cause = e.getCause();
+			if (cause instanceof RuntimeException) throw (RuntimeException) cause;
+			throw new RuntimeException("Unhandled exception in WebApp runner", e);
+		}
+	}
 
-  /*
-   * This may end up needing to be more sophisticated if there is more than one jar on the command line. We would need to looking through
-   * for the one with this class in it.
-   *
-   * @return The main file we are running
-   */
-  protected File getRunFile() {
-    for (URL url : getSystemClassPath()) {
-      if ("file".equals(url.getProtocol()) && (url.getPath().endsWith(".war") || url.getPath().endsWith(".jar"))) {
-        try {
-          return new File(url.toURI());
-        } catch (URISyntaxException e) {
-          /* ignored */
-        }
-      }
-    }
-    throw new RuntimeException("Cannot get the runnable artifact");
-  }
+	/*
+	 * This may end up needing to be more sophisticated if there is more than one jar on the command line. We would need to looking through
+	 * for the one with this class in it.
+	 *
+	 * @return The main file we are running
+	 */
+	protected File getRunFile() {
+		for (URL url : getSystemClassPath()) {
+			if ("file".equals(url.getProtocol()) && (url.getPath().endsWith(".war") || url.getPath().endsWith(".jar"))) {
+				try {
+					return new File(url.toURI());
+				} catch (URISyntaxException e) {
+					/* ignored */
+				}
+			}
+		}
+		throw new RuntimeException("Cannot get the runnable artifact");
+	}
 
-  /*
-   * Returns the URLs that make up the system class path.
-   */
-  protected URL[] getSystemClassPath() {
-    ClassLoader loader = ClassLoader.getSystemClassLoader();
-    if (!(loader instanceof URLClassLoader))
-      throw new RuntimeException("System class loader does not expose classpath URLs");
-    return ((URLClassLoader) loader).getURLs();
-  }
+	/*
+	 * Returns the URLs that make up the system class path.
+	 */
+	protected URL[] getSystemClassPath() {
+		ClassLoader loader = ClassLoader.getSystemClassLoader();
+		if (!(loader instanceof URLClassLoader))
+			throw new RuntimeException("System class loader does not expose classpath URLs");
+		return ((URLClassLoader) loader).getURLs();
+	}
 
 
 }
